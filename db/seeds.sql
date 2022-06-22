a8128b37955679b4949f4bf6b1129c2af46dcd6a
russe433 <russe433@msu.edu>
2022-06-21 20:40:19 -0400

    Added initial files. Connected express ports

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES
+    ('Sylvia', 'Plath', 0),
+    ('Charles', 'Bukowski', 1),
+    ('Ernest', 'Hemmingway', 1),
+    ('Jane', 'Austen', 1),
+    ('Mary', 'Shelly', 0),
+    ('Karl', 'Marx', 0),
+    ('Ayn', 'Rand', 1);
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+//express middleware
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello'
+//     });
+// });
+
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
