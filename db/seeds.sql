030e1dd9616d138459980810768aa603b157cccb
russe433 <russe433@msu.edu>
2022-06-26 21:45:51 -0400

    updated server.js to join party data

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,13 +4,13 @@ INSERT INTO parties (name, description)
     ('AntiWork', 'Late-stage capitalism does not work. It has been proven time and time again.'),
     ('SellOuts', '#BigPharma.');
 
-INSERT INTO candidates (first_name, last_name, industry_connected)
+INSERT INTO candidates (first_name, last_name, party_id, industry_connected)
     VALUES
-    ('Sylvia', 'Plath', 0),
-    ('Charles', 'Bukowski', 1),
-    ('Ernest', 'Hemmingway', 1),
-    ('Jane', 'Austen', 1),
-    ('Mary', 'Shelly', 0),
-    ('Karl', 'Marx', 0),
-    ('Ayn', 'Rand', 1);
+    ('Sylvia', 'Plath', 1, 0),
+    ('Charles', 'Bukowski', 2, 1),
+    ('Ernest', 'Hemmingway', 2, 1),
+    ('Jane', 'Austen', 3, 1),
+    ('Mary', 'Shelly', 2, 0),
+    ('Karl', 'Marx', 2, 0),
+    ('Ayn', 'Rand', 3, 1);
 
